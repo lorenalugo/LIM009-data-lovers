c0c523126b42010f668908861783866d3fdf2965
NikolPanebra <nikol.panebra@gmail.com>
2019-03-18 12:56:00 -0500

    creacion de clases en css

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ let arr = [];
 /*functions*/
 const printData = (arr) => {
 	for(let i = 0; i < arr.length; i++) {
-	  const item = `<div class="col-xs-2 col-md-1 border-box"><img class="block auto"src="${arr[i][1]}" alt="${arr[i][0]}-image"/></div>`;
+	  const item = `<div class="col-xs-2 col-md-1 border-box gray-color"><img class="block auto"src="${arr[i][1]}" alt="${arr[i][0]}-image"/></div>`;
 	  document.getElementById('root').insertAdjacentHTML("beforeend", item); ;	
 	}
 }
