dea45db3c5cb3888fab8b47fea6d07baa4969872
lorena <lugotlorenay@gmail.com>
2019-03-31 21:53:27 -0500

    escribiendo tests y correccion de errores de los tests

--- src/data.js
+++ src/data.js
@@ -1,54 +1,52 @@
-/*gets all champions (id and image)*/
+/* gets all champions (id and image) */
 const getAll = (data) => {
-	let localData = [];
-	for(let i in data) {//data = LOL['data']
-		if(data[i].hasOwnProperty('img')) {
-			localData.push([data[i]['id'], data[i]['img']]);
-		} else {
-			localData.push([data[i]['id'], './assets/placeholder.png']);
-		}
-	}
-	return localData; //localData = [id, img]
+  let localData = [];
+  for (let i in data) {// data = LOL['data']
+    if (data[i].hasOwnProperty('img')) {
+      localData.push([data[i]['id'], data[i]['img']]);
+    } else {
+      localData.push([data[i]['id'], './assets/placeholder.png']);
+    }
+  }
+  return localData; // localData = [id, img]
 };
 
-/*filters the champions by role*/
-const roleFilter = (data, condition) => {//data = LOL['data']
-	let newArr = [];
-	let filteredArray = [];
-	for (let i in data) {
-	newArr = data[i]['tags'].filter((role) => {//newArr may have results if it's true or not if it's false
-		return role === condition;
-	});
-	if(newArr.length !== 0) {
-		filteredArray.push([data[i]['id'], data[i]['splash'], data[i]['info']]);
-	}
-	}
-	return filteredArray;//filteredArr = [id, splash, [info]] (array de arrays)	
-}
-//2dapantalla
-const championDetails = (data, i) => {//
-const championsId = Object.keys(data);//array con nombres de los campeones //championsId = [Aatrox, Ahri, Akali,...,Zyra ] 
-const index = championsId.indexOf(i);//posición de cada propiedad (campeón) e.g.:  [2] = Akali
-//console.log(i);
-//console.log(index);
-const championsValue = Object.values(data);//array con los valores de cada propiedad de cada campeón
-const result = [championsValue[index]['name'], championsValue[index]['title'], 
-		championsValue[index]['splash'], championsValue[index]['blurb'], championsValue[index]
-		['info'], championsValue[index]['stats']];
-return result;// [name, title, imagen, blurb, {info}, {stats}]
-}
-/*sorts champions by skills*/
-const sortData = (data, sortBy, sortOrder) => { //parameters: ([id,splash,[info]], 'attack', ascendent)
-	if(sortOrder === 'descendent') {
-	const descendentSort = data.sort((a, b) => { return b[2][sortBy] - a[2][sortBy]});
-	return descendentSort;//[id,splash,[info]]
-	}
-	else {
-	const ascendentSort = data.sort((a, b) => { return a[2][sortBy] - b[2][sortBy]});
-	return ascendentSort;//[id,splash,[info]]
-	}
-
-}
+/* filters the champions by role */
+const roleFilter = (data, condition) => {// data = LOL['data']
+  let newArr = [];
+  let filteredArray = [];
+  for (let i in data) {
+    newArr = data[i]['tags'].filter((role) => {// newArr may have results if it's true or not if it's false
+      return role === condition;
+    });
+    if (newArr.length !== 0) {
+      filteredArray.push([data[i]['id'], data[i]['splash'], data[i]['info']]);
+    }
+  }
+  return filteredArray;// filteredArr = [id, splash, [info]] (array de arrays)	
+};
+// 2dapantalla
+const championDetails = (data, i) => {
+  const championsId = Object.keys(data);// array con nombres de los campeones //championsId = [Aatrox, Ahri, Akali,...,Zyra ] 
+  const index = championsId.indexOf(i);// posición de cada propiedad (campeón) e.g.:  [2] = Akali
+  const championsValue = Object.values(data);// array con los valores de cada propiedad de cada campeón
+  const result = [championsValue[index]['name'], championsValue[index]['title'], championsValue[index]['splash'], championsValue[index]['blurb'], championsValue[index]['info'], championsValue[index]['stats']];
+  return result;// [name, title, imagen, blurb, {info}, {stats}]
+};
+/* sorts champions by skills */
+const sortData = (data, sortBy, sortOrder) => { // parameters: ([id,splash,[info]], 'attack', ascendent)
+  if (sortOrder === 'descendent') {
+    const descendentSort = data.sort((firstItem, secondItem) => { 
+      return secondItem[2][sortBy] - firstItem[2][sortBy];
+    });
+    return descendentSort;// [id,splash,[info]]
+  } else {
+    const ascendentSort = data.sort((firstItem, secondItem) => { 
+      return firstItem[2][sortBy] - secondItem[2][sortBy];
+    });
+    return ascendentSort;// [id,splash,[info]]
+  }
+};
 
 window.getAll = getAll;
 window.roleFilter = roleFilter;
--- src/main.js
+++ src/main.js
@@ -1,112 +1,98 @@
 /* Manejo del DOM */
-/*vars*/
+/* vars */
 let arr = [];
 let arrFiltered = [];
-/*functions*/
-const printAll = (arr) => {//arr = [[id, img], [id, img], [id, img], [id, img]]
-	for(let i = 0; i < arr.length; i++) {
-		const item = `<div class="col-xs-2 col-md-1 border-box gray-color">
-						<img class="block auto img-png" src="${arr[i][1]}" alt="${arr[i][0]}"/>
-					  </div>`;
-		document.getElementById('root').insertAdjacentHTML("beforeend", item);
-	}
-}
+/* functions */
+const printAll = (arr) => { // arr = [[id, img], [id, img], [id, img], [id, img]]
+  for (let i = 0; i < arr.length; i++) {
+    const item = `<div class="col-xs-2 col-md-1 border-box gray-color"><img class="block auto img-png" src="${arr[i][1]}" alt="${arr[i][0]}"/></div>`;
+    document.getElementById('root').insertAdjacentHTML('beforeend', item);
+  }
+};
 
-const printCard = (roleArr) => {//roleArr = [id,splash,[info]]
-	for (let i = 0; i < roleArr.length; i++) {
-		const str = `<div class="row headline-color auto">
-						<div class="col-xs-12 col-md-3">
-						<figure class="auto">
-							<img class="border-box" src="${roleArr[i][1]}" alt="${roleArr[i][0]}-image" />
-						</figure>
-						</div>
-          				<div class="col-xs-12 col-md-9 border-box content-card">
-          					<h3 class="auto">${roleArr[i][0]}</h3>
-          					<ul>
-          					  <li>Attack: ${roleArr[i][2]['attack']}</li>
-          					  <li>defense: ${roleArr[i][2]['defense']}</li>
-          					  <li>Magic: ${roleArr[i][2]['magic']}</li>
-          					  <li>Difficulty: ${roleArr[i][2]['difficulty']}</li>
-          					</ul>
-          					<button data-champion="${roleArr[i][0]}" class="btn block" type="button">SEE MORE</button>
-          				</div>
-          			</div>`;
-		//row.push(str);
-		document.getElementById('role-container').insertAdjacentHTML("beforeend", str);
-	}
-	//document.getElementById('role-container').innerHTML = row;
-}
+const printCard = (roleArr) => {// roleArr = [id,splash,[info]]
+  for (let i = 0; i < roleArr.length; i++) {
+    const str = `<div class="row headline-color auto"><div class="col-xs-12 col-md-3"><figure class="auto"><img class="border-box" src="${roleArr[i][1]}" alt="${roleArr[i][0]}-image" /></figure></div><div class="col-xs-12 col-md-9 border-box content-card"><h3 class="auto">${roleArr[i][0]}</h3><ul><li>Attack: ${roleArr[i][2]['attack']}</li><li>defense: ${roleArr[i][2]['defense']}</li><li>Magic: ${roleArr[i][2]['magic']}</li><li>Difficulty: ${roleArr[i][2]['difficulty']}</li></ul><button data-champion="${roleArr[i][0]}" class="btn block" type="button">SEE MORE</button></div></div>`;
+    // row.push(str);
+    document.getElementById('role-container').insertAdjacentHTML('beforeend', str);
+  }
+};
 
-//shows and hides a block element
+// shows and hides a block element
 const displayBlock = (showId, hideId) => {
-	document.getElementById(hideId).classList.add('none');
-	document.getElementById(showId).classList.remove('none');
-}
-/*events*/
-document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById(hideId).classList.add('none');
+  document.getElementById(showId).classList.remove('none');
+};
+/* events */
+document.addEventListener('DOMContentLoaded', () => {
   arr = getAll(LOL['data']);
   printAll(arr);
 });
-/*menu buttons events*/
+/* menu buttons events */
 const assassinBtn = document.getElementById('assassin');
 assassinBtn.addEventListener('click', () => {
-	document.getElementById('role-container').innerHTML = "";
-	arrFiltered = roleFilter(LOL['data'], 'Assassin');
-	printCard(arrFiltered);
-	displayBlock('role-container', 'root');
-	displayBlock('role-container', 'detailsContainer');//(muestra, oculta)
+  document.getElementById('role-container').innerHTML = '';
+  document.getElementById('submenu').classList.remove('none');
+  arrFiltered = roleFilter(LOL['data'], 'Assassin');
+  printCard(arrFiltered);
+  displayBlock('role-container', 'root');
+  displayBlock('role-container', 'detailsContainer');// (muestra, oculta)
 });
 
 const fighterBtn = document.getElementById('fighter');
 fighterBtn.addEventListener('click', () => {
-	document.getElementById('role-container').innerHTML = "";
-	arrFiltered = roleFilter(LOL['data'], 'Fighter');
-	printCard(arrFiltered);
-	displayBlock('role-container', 'root');	
-	displayBlock('role-container', 'detailsContainer');
+  document.getElementById('role-container').innerHTML = '';
+  document.getElementById('submenu').classList.remove('none');
+  arrFiltered = roleFilter(LOL['data'], 'Fighter');
+  printCard(arrFiltered);
+  displayBlock('role-container', 'root');	
+  displayBlock('role-container', 'detailsContainer');
 });
 
 const mageBtn = document.getElementById('mage');
 mageBtn.addEventListener('click', () => {
-	document.getElementById('role-container').innerHTML = "";
-	arrFiltered = roleFilter(LOL['data'], 'Mage');
-	printCard(arrFiltered);
-	displayBlock('role-container', 'root');
-	displayBlock('role-container', 'detailsContainer');
+  document.getElementById('role-container').innerHTML = '';
+  document.getElementById('submenu').classList.remove('none');
+  arrFiltered = roleFilter(LOL['data'], 'Mage');
+  printCard(arrFiltered);
+  displayBlock('role-container', 'root');
+  displayBlock('role-container', 'detailsContainer');
 });
 
 const marksmanBtn = document.getElementById('marksman');
 marksmanBtn.addEventListener('click', () => {
-	document.getElementById('role-container').innerHTML = "";
-	arrFiltered = roleFilter(LOL['data'], 'Marksman');
-	printCard(arrFiltered);
-	displayBlock('role-container', 'root');	
-	displayBlock('role-container', 'detailsContainer');
+  document.getElementById('role-container').innerHTML = '';
+  document.getElementById('submenu').classList.remove('none');
+  arrFiltered = roleFilter(LOL['data'], 'Marksman');
+  printCard(arrFiltered);
+  displayBlock('role-container', 'root');	
+  displayBlock('role-container', 'detailsContainer');
 });
 
 const tankBtn = document.getElementById('tank');
 tankBtn.addEventListener('click', () => {
-	document.getElementById('role-container').innerHTML = "";
-	arrFiltered = roleFilter(LOL['data'], 'Tank');
-	printCard(arrFiltered);
-	displayBlock('role-container', 'root');	
-	displayBlock('role-container', 'detailsContainer');
+  document.getElementById('role-container').innerHTML = '';
+  document.getElementById('submenu').classList.remove('none');
+  arrFiltered = roleFilter(LOL['data'], 'Tank');
+  printCard(arrFiltered);
+  displayBlock('role-container', 'root');	
+  displayBlock('role-container', 'detailsContainer');
 });
 
 const supportBtn = document.getElementById('support');
 supportBtn.addEventListener('click', () => {
-	document.getElementById('role-container').innerHTML = "";
-	arrFiltered = roleFilter(LOL['data'], 'Support');
-	printCard(arrFiltered);
-	displayBlock('role-container', 'root');	
-	displayBlock('role-container', 'detailsContainer');
+  document.getElementById('role-container').innerHTML = '';
+  document.getElementById('submenu').classList.remove('none');
+  arrFiltered = roleFilter(LOL['data'], 'Support');
+  printCard(arrFiltered);
+  displayBlock('role-container', 'root');	
+  displayBlock('role-container', 'detailsContainer');
 });
 
-/* mostrar 2da pantalla*/
+/* mostrar 2da pantalla */
 
-let profile = [];
 const printDetails = (profile) => { // [name, title, img, blurb, {info}, {stats} ] // copia todos los elementos, sean arrays o contenido de objetos
-const info =`<div class="col-xs-12 profiles border-box">
+  const info = `<div class="col-xs-12 profiles border-box">
 				<div>
 				<h2 class="in-line">${profile[0]}</h2>
 				<button class="btn float-right"> HOME </button>
@@ -119,7 +105,6 @@ const info =`<div class="col-xs-12 profiles border-box">
 				<p class="blurb">${profile[3]}</p>
 				</figure>
 				</div>	
-
 				<div class="white-color col-xs-12 col-md-8 description border-box"> 
 				<p>Attack: ${profile[4]['attack']}</p>
 				<p>Defense: ${profile[4]['defense']}</p>
@@ -141,7 +126,6 @@ const info =`<div class="col-xs-12 profiles border-box">
 				<p>Spell block per level: ${profile[5]['spellblockperlevel']}</p>
 				<p>Attack range: ${profile[5]['attackrange']}</p>
 				</div>
-
 				<div class="col-xs-12 col-md-6">
 				<p>HP Regeneration Points: ${profile[5]['hpregen']}</p>
 				<p>HP Regeneration Points per level: ${profile[5]['hpregenperlevel']}</p>
@@ -155,36 +139,35 @@ const info =`<div class="col-xs-12 profiles border-box">
 				<p>Attack speed per level: ${profile[5]['attackspeedperlevel']}</p>
 				</div>
 				</div>
-
 			</div>`;
-document.getElementById('detailsContainer').innerHTML = info;
+  document.getElementById('detailsContainer').innerHTML = info;
 };
 	
-/*end menu buttons*/
+/* end menu buttons */
 const gridClickImg = document.getElementById('root');
-gridClickImg.addEventListener('click', (e) => {
-	//console.log(e.target.alt); //porque el value proviene del object-keys (nombres de las propiedades del objeto data)
-	const profile = championDetails(LOL['data'], e.target.alt);
-    printDetails(profile);
-    displayBlock ('detailsContainer', 'root');
+gridClickImg.addEventListener('click', (event) => {
+  // console.log(e.target.alt); //porque el value proviene del object-keys (nombres de las propiedades del objeto data)
+  const profile = championDetails(LOL['data'], event.target.alt);
+  printDetails(profile);
+  displayBlock('detailsContainer', 'root');
 });
 
 const seeMoreBtn = document.getElementById('role-container');
-seeMoreBtn.addEventListener('click', (e) => {
-	const profile = championDetails(LOL['data'], e.target.dataset.champion);
-    printDetails(profile);
-    displayBlock ('detailsContainer', 'role-container');
+seeMoreBtn.addEventListener('click', (event) => {
+  const profile = championDetails(LOL['data'], event.target.dataset.champion);
+  printDetails(profile);
+  displayBlock('detailsContainer', 'role-container');
 });
 
 const sortMenuBtn = document.getElementById('sort-menu');
-sortMenuBtn.addEventListener('click', (e) => {
-	document.getElementById('role-container').innerHTML = "";
-	const sortResult = sortData(arrFiltered, e.target.dataset.skill, 'descendent');
-    //console.log(sortResult);
-    printCard(sortResult);
-    document.getElementById('sort-condition').innerHTML = e.target.dataset.skill;
-    displayBlock('role-container', 'root');	
-	displayBlock('role-container', 'detailsContainer');
+sortMenuBtn.addEventListener('click', (event) => {
+  document.getElementById('role-container').innerHTML = '';
+  const sortResult = sortData(arrFiltered, event.target.dataset.skill, 'descendent');
+  // console.log(sortResult);
+  printCard(sortResult);
+  document.getElementById('sort-condition').innerHTML = event.target.dataset.skill;
+  displayBlock('role-container', 'root');	
+  displayBlock('role-container', 'detailsContainer');
 });
 
 
